86e1e4e0ffb173288b9c8b4e49a7c3b36b0a8ee7
khalilurrahmanpro <mdkhlilurrahmanpro@gmail.com>
2025-06-10 15:18:35 +0600

    first commit

---
+++ result.js
@@ -0,0 +1,21 @@
+const result = 10;
+if(result >= 0 && result <40 ){
+    console.log('faild');
+}else if(result >= 40 && result <50){
+    console.log('he got grade: C');
+}else if(result >= 50 & result <60){
+    console.log('he got grade: B');
+}
+else if(result >= 60 & result <70){
+    console.log('he got grade: B+');
+}
+else if(result >= 70 & result <80){
+    console.log('he got grade: A');
+}
+else if(result >= 80 & result <=100){
+    console.log('he got grade: A+');
+}
+else{
+    console.log('invaild');
+}
+
